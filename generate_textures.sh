652edd45476523630a91bb2a877df4d34e1f235f
Pau Ruiz i Safont <unduthegun@gmail.com>
2016-12-08 23:16:08 +0000

    Use sh scripts instead of batch ones
    Windows has bash anyway :)

---
+++ generate_textures.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+folder='textures/workshop'
+if [ ! -d "$folder" ]; then
+  mkdir -p "$folder"
+fi
+
+
+sh ./textures/scripts/gradients_gen.sh
+for img in img/*.png; do
+  echo Processing "${img##*/}"
+  sh ./textures/scripts/texture_emblems.sh "$img" textures
+done
+
+sh ./common_scripts/montage.sh textures/dds textures/workshop 5x
+
+rm textures/temp/gradient_fill.png
+rm textures/temp/gradient_highlight.png
+
+rm -rf textures/temp
---
+++ textures/scripts/texture_emblems.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env sh
+# $1 is the image path
+# $2 is the "directory prefix"
+
+fileExt="${1##*/}"
+file="${fileExt%.*}"
+folder="$2"
+
+# create necessary folders
+for folder in 'textures/temp' "textures/dds/small" "textures/dds/map";  do
+  if [ ! -d "$folder" ]; then
+    mkdir -p "$folder"
+  fi
+done
+
+#gradient fill
+magick convert $1 -quiet -alpha copy \
+       "${2}/temp/gradient_fill.png" -compose atop -composite \
+       "${2}/temp/${file}_fill.png"
+
+#highlights
+magick convert $1 -quiet -morphology HMT \
+       "6x6+0+5:0,-,-,-,-,- -,-,-,-,-,- -,-,-,-,-,- -,-,-,-,-,- -,-,-,-,-,- 1,-,-,-,-,-; 3x3+2+0:0,-,1 -,-,- -,-,-" \
+       -alpha copy \
+       $2/temp/gradient_highlight.png -compose atop -composite \
+       "${2}/temp/${file}_highlights.png"
+
+magick convert "$2/temp/${file}_fill.png" \
+       "${2}/temp/${file}_highlights.png" -compose screen -composite \
+       -sigmoidal-contrast 2,50%% \
+       -background white \
+       "${2}/temp/${file}.png"
+
+# borders
+magick convert \( $1 -morphology EdgeOut Octagon:3 -alpha copy \) \
+       \( -size 512x512 canvas:black \) -compose atop -composite \
+       "${2}/temp/${file}_border.png"
+
+# default emblems
+magick convert "${2}/temp/${file}.png" \
+       -colorspace RGB -resize 128x128 -colorspace sRGB \
+       -define dds:compression=dxt5 -define dds:cluster-fit=true -define dds:weight-by-alpha=true \
+       "${2}/dds/${file}.dds"
+
+# small emblems
+magick convert "${2}/temp/${file}.png" \
+       -fill black -colorize 100%% -channel RGBA -blur 32x32 -level 0,97%% \
+       "${2}/temp/${file}.png" -compose Over -composite \
+       "${2}/temp/${file}_border.png" -compose Dst_Over -composite \
+       -colorspace RGB -resize 24x24 -colorspace sRGB \
+       -define dds:compression=none \
+       "${2}/dds/small/${file}.dds"
+
+#map emblems
+magick convert "$1" -alpha copy \
+       -resize 256x256 \
+       -define dds:compression=dxt5 -define dds:cluster-fit=true -define dds:weight-by-alpha=true \
+       "${2}/dds/map/${file}.dds"
+
+rm "${2}/temp/${file}_fill.png"
+rm "${2}/temp/${file}_highlights.png"
+rm "${2}/temp/${file}_border.png"
+rm "${2}/temp/${file}.png"
